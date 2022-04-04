88ecf7bc43285501c283941c136374ec433c2079
Nakyejwe <aidahnakyejwe578@gmail.com>
2022-04-04 17:26:04 +0300

    first assignemnt

--- docker/app.py
+++ docker/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Welcome to SCA Cloud School Application'
+    return 'Welcome to SCA Cloud School Application, This is my first asssesment'
 if __name__=="__main__":
     app.run(debug=True)
